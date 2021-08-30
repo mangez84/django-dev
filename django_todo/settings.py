ff6d4daae7975df82b027f06e76b024471f20f1d
Magnus Rosén <mangez84@gmail.com>
2021-08-30 20:43:08 +0000

    Fix allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-($#4fhgejw6$_&h3j0-&^n%z3fksz-9r+mgo3f@lpqg3)#ho1(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mangez84-django-todo-app.herokuapp.com']
 
 
 # Application definition
